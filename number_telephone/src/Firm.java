96a071d16a7ccb207490a9474d96a045ef946c0f
245965 <245965@edu.p.lodz.pl>
2024-03-12 13:03:45 +0100

    Createing Clesses and Interface

---
+++ number_telephone/src/Firm.java
@@ -0,0 +1,2 @@
+public class Firm extends Post{
+}
---
+++ number_telephone/src/NumberTelephone.java
@@ -0,0 +1,5 @@
+public class NumberTelephone implements IComparable {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
